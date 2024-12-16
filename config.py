c17154713c38f85fa63a879d9f7c8f4f8bef4792
Shubham Gautam <46371393+ichbingautam@users.noreply.github.com>
2024-12-16 16:58:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ class Config:
     SQLALCHEMY_DATABASE_URI = 'postgresql://@localhost/video_store'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     YOUTUBE_API_KEYS = ["YOUR_API_KEY_1", "YOUR_API_KEY_2"]
-    SEARCH_QUERY = "Python"
+    SEARCH_QUERY = "cricket"
     FETCH_INTERVAL = 10  # in seconds
